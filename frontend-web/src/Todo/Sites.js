2fb6539f967c52a280897b6d662fdc5ae93345a8
Kane Clover <baron.vonkanehoffen@gmail.com>
2019-12-26 14:18:27 +0000

    copy Hasura todo-auth0-jwt example

---
+++ frontend-web/src/Todo/Sites.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Query } from "react-apollo";
+import gql from 'graphql-tag';
+import Todo from './Todo'
+
+const QUERY_SITES = gql`
+    query sites_near_point($point: geography!){
+        site(
+            where: {location: {_st_d_within: {distance: 2000, from: $point}}}
+        ){
+            id
+            location
+            name
+        }
+    }
+`;
+
+const queryVars = {
+  "point": {
+    "type": "Point",
+    "coordinates": [ 12.939553, 77.6183303 ]
+  }
+};
+
+export default function Sites () {
+  return (
+    <Query query={QUERY_SITES} variables={queryVars}>
+      {({loading, error, data}) => {
+        if (loading) {
+          return (
+            <div>Loading. Please wait...</div>
+          );
+        }
+        if (error) {
+          return (
+            <div>
+              <h3>Error:</h3>
+              <pre>{JSON.stringify(error, null, 2)}</pre>
+            </div>
+          );
+        }
+        return (
+          <div className="parentContainer">
+            <h3>Sites query:</h3>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+          </div>
+        )
+      }}
+    </Query>
+  )
+};
---
+++ frontend-web/src/constants.js
@@ -0,0 +1,3 @@
+export const GRAPHQL_URL = 'https://planning-alerts.herokuapp.com/v1/graphql';
+export const authClientId = "WzXYaLA48ZlJPGtrCMHfWmw9ytopwI9E";
+export const authDomain = "kanec.eu.auth0.com";
