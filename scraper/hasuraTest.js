9d8c105cc164abfb1458199d493f9260a352bf65
Kane Clover <baron.vonkanehoffen@gmail.com>
2020-02-05 21:29:29 +0000

    working apollo link test (so gql tags work)

--- scraper/hasuraTest.js
+++ scraper/hasuraTest.js
@@ -1,25 +1,45 @@
-const { apolloFetch } = require('./src/lib/apolloFetch');
+const { execute, makePromise } = require("apollo-link");
+const { HttpLink } = require("apollo-link-http");
+const config = require("./config");
 const gql = require("graphql-tag");
+const fetch = require("node-fetch");
 
-const query = `
-    query get_planning_app_by_id($id: String!) {
+const link = new HttpLink({
+  uri: config.hasuraApi,
+  headers: { 'x-hasura-admin-secret': config.hasuraAdminSecret },
+  fetch: fetch
+});
+
+// const { apolloFetch } = require('./src/lib/apolloFetch');
+
+const query = gql`
+  query get_planning_app_by_id($id: String!) {
     pa_status_by_pk(id: $id) {
       id
-      proposal 
+      proposal
     }
   }
 `;
-// TODO: ....but not when it's gql tagged.
 
-console.log(query);
+const variables = {
+  id: "A1234/23"
+};
+
+// console.log(JSON.stringify(query, null, 2));
+
 async function whatever() {
-  const resposne = await apolloFetch({
-    query,
-    variables: {
-      "id": "A1234/23"
-    }
-  });
-  console.log(resposne);
+  // const response = await apolloFetch({
+  //   query,
+  //   variables
+  // });
+  const response = await makePromise(
+    execute(link, {
+      query,
+      variables
+    })
+  );
+
+  console.log(response);
 }
 
 whatever();
