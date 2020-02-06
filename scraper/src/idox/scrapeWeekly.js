c81be67a727996caa616d6b16f8864e2fa9b7481
Kane Clover <baron.vonkanehoffen@gmail.com>
2020-02-06 23:56:52 +0000

    Log general scrape errors to Hasura

--- scraper/src/idox/scrapeWeekly.js
+++ scraper/src/idox/scrapeWeekly.js
@@ -26,99 +26,110 @@ async function scrapeWeekly(rootURL) {
 async function scrapeFullList(root, listType) {
   const rootURL = new URL(root);
 
-  // Get latest list date
+  try {
+    // Get latest list date
 
-  const searchForm = await request({
-    uri: `${rootURL}/search.do?action=weeklyList&searchType=Application`
-  });
-  const latestListDate = searchForm("select#week option")
-    .first()
-    .attr("value");
-  console.log(latestListDate);
+    const searchForm = await request({
+      uri: `${rootURL}/search.do?action=weeklyList&searchType=Application`
+    });
+    const latestListDate = searchForm("select#week option")
+      .first()
+      .attr("value");
+    console.log(latestListDate);
 
-  // Get request args from the form
+    // Get request args from the form
 
-  const firstUri = searchForm("form[name=searchCriteriaForm]").attr("action");
-  const firstPageUri = `${rootURL.origin}${firstUri}`;
-  let params = searchForm("form[name=searchCriteriaForm]").serialize();
-  params = params.replace(/dateType=.*&/, `dateType=${listType}&`);
-  console.log(params);
-  console.log("URI: ", firstPageUri);
+    const firstUri = searchForm("form[name=searchCriteriaForm]").attr("action");
+    const firstPageUri = `${rootURL.origin}${firstUri}`;
+    let params = searchForm("form[name=searchCriteriaForm]").serialize();
+    params = params.replace(/dateType=.*&/, `dateType=${listType}&`);
+    console.log(params);
+    console.log("URI: ", firstPageUri);
 
-  // Get first page of the chosen list type
+    // Get first page of the chosen list type
 
-  let listPage = await request({
-    uri: firstPageUri,
-    method: "POST",
-    body: params
-  });
-  let detailURLs = [];
-  detailURLs = getDetailURLs(listPage);
-  if (detailURLs.length < 1) {
-    console.log("Single app only");
-    detailURLs = [firstUri];
-  }
+    let listPage = await request({
+      uri: firstPageUri,
+      method: "POST",
+      body: params
+    });
+    let detailURLs = [];
+    detailURLs = getDetailURLs(listPage);
+    if (detailURLs.length < 1) {
+      console.log("Single app only");
+      detailURLs = [firstUri];
+    }
 
-  console.log(detailURLs);
+    console.log(detailURLs);
 
-  // Scrape PA detail, looping over pagination
-  while (detailURLs.length > 0) {
-    for (const detailURL of detailURLs) {
-      const uri = `${rootURL.origin}${detailURL}`;
-      let scrape = {
-        list_type: listType,
-        scraper: "idox",
-        url: uri
-      };
+    // Scrape PA detail, looping over pagination
+    while (detailURLs.length > 0) {
+      for (const detailURL of detailURLs) {
+        const uri = `${rootURL.origin}${detailURL}`;
+        let scrape = {
+          list_type: listType,
+          scraper: "idox",
+          url: uri
+        };
 
-      // Scrape summary
-      const summaryPage = await request({
-        uri
-      });
-      scrape.summary = scrapeTableData(summaryPage);
-      scrape.reference = scrape.summary.reference;
-
-      // If there's no reference, something's gone wrong...
-      if (!scrape.reference) {
-        await storeScrapeLog("idox", "NO_SCRAPED_REFERENCE", {
-          scrape,
-          html: summaryPage.html()
+        // Scrape summary
+        const summaryPage = await request({
+          uri
         });
-        continue;
+        scrape.summary = scrapeTableData(summaryPage);
+        scrape.reference = scrape.summary.reference;
+
+        // If there's no reference, something's gone wrong...
+        // TODO: This needs to handle 302 / network errors as well. Currently blows...
+        if (!scrape.reference) {
+          await storeScrapeLog("idox", "NO_SCRAPED_REFERENCE", {
+            scrape,
+            html: summaryPage.html()
+          });
+          continue;
+        }
+
+        // Scrape further info
+        const furtherInfoUri = summaryPage("#subtab_details").attr("href");
+        const furtherInfoPage = await request({
+          uri: `${rootURL.origin}${furtherInfoUri}`
+        });
+        scrape.further_information = scrapeTableData(furtherInfoPage);
+
+        // Scrape dates
+        const datesUri = summaryPage("#subtab_dates").attr("href");
+        const datesPage = await request({
+          uri: `${rootURL.origin}${datesUri}`
+        });
+        scrape.important_dates = scrapeTableData(datesPage);
+
+        // Scrape contacts
+        // TODO: Needs different scrape and is on main tab (but always blank?) for westminster
+        //  Also do we need it? Agent name is on Further info tab, so this is just email addresses.
+
+        console.log("scrape:", scrape);
+        await storeScrape(scrape);
       }
 
-      // Scrape further info
-      const furtherInfoUri = summaryPage("#subtab_details").attr("href");
-      const furtherInfoPage = await request({
-        uri: `${rootURL.origin}${furtherInfoUri}`
-      });
-      scrape.further_information = scrapeTableData(furtherInfoPage);
-
-      // Scrape dates
-      const datesUri = summaryPage("#subtab_dates").attr("href");
-      const datesPage = await request({ uri: `${rootURL.origin}${datesUri}` });
-      scrape.important_dates = scrapeTableData(datesPage);
-
-      // Scrape contacts
-      // TODO: Needs different scrape and is on main tab (but always blank?) for westminster
-      //  Also do we need it? Agent name is on Further info tab, so this is just email addresses.
-
-      console.log("scrape:", scrape);
-      await storeScrape(scrape);
+      // Get next page of the list
+      const nextLink = listPage("#searchResultsContainer a.next")
+        .first()
+        .attr("href");
+      if (nextLink) {
+        console.log("Next page: ", nextLink);
+        listPage = await request({ uri: `${rootURL.origin}${nextLink}` });
+        detailURLs = getDetailURLs(listPage);
+      } else {
+        console.log("All pages scraped.");
+        detailURLs = [];
+      }
     }
-
-    // Get next page of the list
-    const nextLink = listPage("#searchResultsContainer a.next")
-      .first()
-      .attr("href");
-    if (nextLink) {
-      console.log("Next page: ", nextLink);
-      listPage = await request({ uri: `${rootURL.origin}${nextLink}` });
-      detailURLs = getDetailURLs(listPage);
-    } else {
-      console.log("All pages scraped.");
-      detailURLs = [];
-    }
+  } catch (e) {
+    await storeScrapeLog("idox", "SCRAPE_LIST_ERROR", {
+      root,
+      listType,
+      message: e.message
+    });
   }
   // return;
   // await getAllApplicationDetails(rootURL, detailURLs);
