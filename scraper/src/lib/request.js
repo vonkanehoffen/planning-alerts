70739dbd907bb5ae28ce8c83339242ed497c2aa0
Kane Clover <baron.vonkanehoffen@gmail.com>
2020-02-06 23:26:13 +0000

    request debug log + follow POST redirects

--- scraper/src/lib/request.js
+++ scraper/src/lib/request.js
@@ -1,7 +1,11 @@
 var rp = require("request-promise");
 var cheerio = require("cheerio"); // Basically jQuery for node.js
+var config = require("../../config");
 
-// require('request-debug')(rp);
+if (config.debug) {
+  // require('request-debug')(rp);
+  rp.debug = true;
+}
 
 const j = rp.jar();
 var request = rp.defaults({
@@ -13,7 +17,8 @@ var request = rp.defaults({
     "User-Agent":
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36"
   },
-  jar: j
+  jar: j,
+  followAllRedirects: true // follow non-GET HTTP 3xx responses as redirects
 });
 
 exports.request = request;
