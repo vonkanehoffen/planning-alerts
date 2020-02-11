c81a6e8056b9c73ddaf4c18e6d6cd5223601e3e6
Kane Clover <baron.vonkanehoffen@gmail.com>
2020-02-11 23:20:04 +0000

    Fetch markers from component inside map view to stop re-rendering of entire map on change

--- frontend-mobile/src/screens/home/pa-map.component.js
+++ frontend-mobile/src/screens/home/pa-map.component.js
@@ -1,120 +1,90 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import {StyleSheet} from 'react-native';
+import {Marker} from 'react-native-maps';
 import * as queries from '../../data-layer/graphql-queries';
-import { Spinner, Layout, Text } from '@ui-kitten/components';
-import { useQuery } from '@apollo/react-hooks'
-import { subDays, formatISO } from 'date-fns';
+import {useQuery} from '@apollo/react-hooks';
+import {subDays, formatISO} from 'date-fns';
 
-export function PaMap ({ userLocation }) {
-  const [queryLocation, setQueryLocation] = React.useState(userLocation);
+export function PaMap({userLocation}) {
 
   const {
     loading: openPaLoading,
     error: openPaError,
-    data: openPaData
+    data: openPaData,
   } = useQuery(queries.GET_OPEN_PA_NEAR_POINT, {
     variables: {
-      point: queryLocation,
-      distance: 2000
-    }
+      point: userLocation,
+      distance: 2000,
+    },
     // skip: !userLocation
   });
 
-  const minDate = formatISO(subDays(new Date(), 3), { representation: 'date' });
+  const minDate = formatISO(subDays(new Date(), 3), {representation: 'date'});
   const {
     loading: closedPaLoading,
     error: closedPaError,
-    data: closedPaData
+    data: closedPaData,
   } = useQuery(queries.GET_RECENT_CLOSED_PA_NEAR_POINT, {
     variables: {
-      point: queryLocation,
+      point: userLocation,
       distance: 2000,
-      minDate: minDate
-    }
+      minDate: minDate,
+    },
   });
 
-  const initialRegion = {
-    latitude: userLocation.coordinates[0],
-    longitude: userLocation.coordinates[1],
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  };
+  console.log("RENDER PA MAP PINS", { openPaLoading, openPaError, openPaData, closedPaLoading,closedPaError, closedPaData});
 
-  /**
-   * Update location gql query param with new map region, when dragged.
-   * TODO: This redraws the whole map.... recompose.
-   * @param region
-   */
-  const doSetQueryLocation = (region) => {
-    setQueryLocation({
-      type: "Point",
-      coordinates: [region.latitude, region.longitude]
-    });
-  }
-
+  // TODO: Loading and error display for this further up the tree.
   if (openPaLoading || closedPaLoading) {
-    return <Layout><Spinner/></Layout>;
+    return false;
+    // return (
+    //   <Layout>
+    //     <Spinner />
+    //   </Layout>
+    // );
   }
 
   if (openPaError || closedPaError) {
-    return (
-      <Layout>
-        <Text statu="danger">{openPaError} {closedPaError}</Text>
-      </Layout>
-    );
+    return false;
+    // return (
+    //   <Layout>
+    //     <Text statu="danger">
+    //       {openPaError} {closedPaError}
+    //     </Text>
+    //   </Layout>
+    // );
   }
 
   console.log({
-    queryLocation,
+    userLocation,
     openPaData,
     closedPaData,
   });
 
   return (
-    <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-        style={styles.map}
-        initialRegion={initialRegion}
-        onRegionChangeComplete={doSetQueryLocation}
-      >
-        {closedPaData.pa_status.map(pa => (
-          <Marker
-            coordinate={{
-              latitude: pa.location.coordinates[0],
-              longitude: pa.location.coordinates[1]
-            }}
-            key={pa.id}
-            title={pa.id}
-            description={pa.proposal}
-          />
-        ))}
-        {openPaData.pa_status.map(pa => (
-          <Marker
-            coordinate={{
-              latitude: pa.location.coordinates[0],
-              longitude: pa.location.coordinates[1]
-            }}
-            key={pa.id}
-            title={pa.id}
-            description={pa.proposal}
-          />
-        ))}
-      </MapView>
-    </View>
+    <>
+      {closedPaData.pa_status.map(pa => (
+        <Marker
+          coordinate={{
+            latitude: pa.location.coordinates[0],
+            longitude: pa.location.coordinates[1],
+          }}
+          key={pa.id}
+          title={pa.id}
+          description={pa.proposal}
+        />
+      ))}
+      {openPaData.pa_status.map(pa => (
+        <Marker
+          coordinate={{
+            latitude: pa.location.coordinates[0],
+            longitude: pa.location.coordinates[1],
+          }}
+          key={pa.id}
+          title={pa.id}
+          description={pa.proposal}
+        />
+      ))}
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-});
--- frontend-mobile/src/screens/home/user-location.component.js
+++ frontend-mobile/src/screens/home/user-location.component.js
@@ -1,21 +1,30 @@
-import React, { useContext } from "react";
-import { useQuery } from "@apollo/react-hooks";
-import * as queries from '../../data-layer/graphql-queries'
-import { Layout, Text, Spinner } from '@ui-kitten/components'
-import { PaMap } from './pa-map.component'
-import _ from "lodash";
-import { AuthContext } from '../auth/auth-provider.component'
+import React, {useContext} from 'react';
+import {useQuery} from '@apollo/react-hooks';
+import * as queries from '../../data-layer/graphql-queries';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {Layout, Text, Spinner} from '@ui-kitten/components';
+import {PaMap} from './pa-map.component';
+import _ from 'lodash';
+import {AuthContext} from '../auth/auth-provider.component';
+import {StyleSheet, View} from 'react-native';
 
-export function UserLocation({ navigation }) {
-  const { auth } = useContext(AuthContext)
-  const { loading, error, data } = useQuery(queries.GET_USER_LOCATION, {
+export function UserLocation({navigation}) {
+  const {auth} = useContext(AuthContext);
+  const {loading, error, data} = useQuery(queries.GET_USER_LOCATION, {
     variables: {
-      id: auth.userInfo.sub
-    }
+      id: auth.userInfo.sub,
+    },
   });
 
+  const doLocationChange = (region) => console.log("REGION CHANGE", region);
+
+  console.log("RENDER USERLOC", {loading, error, data});
   if (loading) {
-    return <Layout><Spinner/></Layout>;
+    return (
+      <Layout>
+        <Spinner />
+      </Layout>
+    );
   }
 
   if (error) {
@@ -26,12 +35,51 @@ export function UserLocation({ navigation }) {
     );
   }
 
-  const location = _.get(data, "users[0].location");
+  /**
+   * Update location gql query param with new map region, when dragged.
+   * @param region
+   */
+  // const doSetQueryLocation = (region) => {
+  //   setQueryLocation({
+  //     type: "Point",
+  //     coordinates: [region.latitude, region.longitude]
+  //   });
+  // }
+
+  const location = _.get(data, 'users[0].location');
 
   if (!location) {
-    navigation.navigate("set-location");
+    navigation.navigate('set-location');
     return false;
   }
 
-  return <PaMap userLocation={location} />;
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: location.coordinates[0],
+          longitude: location.coordinates[1],
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        onRegionChangeComplete={doLocationChange}>
+        <PaMap userLocation={location} />
+      </MapView>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
