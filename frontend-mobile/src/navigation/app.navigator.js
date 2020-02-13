c7e004ee22cfb7b62cf2c62b7efc9d61bd478105
Kane Clover <baron.vonkanehoffen@gmail.com>
2020-02-13 22:04:45 +0000

    Settings icon nav on top bar

--- frontend-mobile/src/navigation/app.navigator.js
+++ frontend-mobile/src/navigation/app.navigator.js
@@ -1,16 +1,35 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { HomeScreen } from "../screens/home/home-screen.component";
 import { SettingsScreen } from "../screens/settings/settings-screen.component";
+import { Icon } from "@ui-kitten/components";
+import { TouchableOpacity } from "react-native";
 
 const Stack = createStackNavigator();
 
-export const AppNavigator = () => (
-  <NavigationContainer>
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Settings" component={SettingsScreen} />
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+export function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerRight: () => <SettingsButton />
+          }}
+        />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function SettingsButton() {
+  const navigation = useNavigation();
+  return (
+    <TouchableOpacity onPress={() => navigation.navigate("Settings")}>
+      <Icon name="settings-outline" width={32} height={32} fill="#3366FF" />
+    </TouchableOpacity>
+  );
+}
