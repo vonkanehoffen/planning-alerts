bf8648134946cc6feab4371454516986111c0825
Kane Clover <baron.vonkanehoffen@gmail.com>
2020-02-25 21:34:59 +0000

    update config example

--- scraper/config.example.js
+++ scraper/config.example.js
@@ -5,5 +5,11 @@ module.exports = {
   debug: true,
   slackWebHookURL:
     "https://hooks.slack.com/services/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",
-  sentryDSN: "https://SOMETHING@sentry.io/SOMETHING"
+  sentryDSN: "https://SOMETHING@sentry.io/SOMETHING",
+  firebaseServiceAccountKey: {
+    "type": "service_account",
+    "project_id": "planning-alerts",
+    // ...
+  }
+
 };
