b4989aa42f76dcebbb6fbccbc3e5616e6e292f1e
Kane Clover <baron.vonkanehoffen@gmail.com>
2020-03-12 14:56:38 +0000

    Logo and spinner top of map

--- frontend-mobile/src/screens/home/home-screen.component.js
+++ frontend-mobile/src/screens/home/home-screen.component.js
@@ -1,14 +1,27 @@
 import React, { useEffect } from "react";
-import { StyleSheet } from "react-native";
-import { Layout } from "@ui-kitten/components";
+import { StyleSheet, View } from "react-native";
+import { Layout, useTheme, Spinner } from "@ui-kitten/components";
 import { UserLocationMap } from "./user-location-map.component";
 import { FCMSetup } from "../../data-layer/fcm-setup.component";
+import { PaLogo } from "../../components/pa-logo.component";
+import { useApolloNetworkStatus } from "react-apollo-network-status";
 
 export function HomeScreen({ navigation }) {
+  const theme = useTheme();
+  const status = useApolloNetworkStatus();
+
   return (
     <Layout style={styles.container}>
       <FCMSetup />
       <UserLocationMap navigation={navigation} />
+      <View style={styles.logo}>
+        <PaLogo size={40} color={theme["color-primary-900"]} />
+      </View>
+      {(status.numPendingQueries > 0 || status.numPendingMutations > 0) && (
+        <View style={styles.loading}>
+          <Spinner />
+        </View>
+      )}
     </Layout>
   );
 }
@@ -18,5 +31,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  logo: {
+    position: "absolute",
+    top: 10,
+    left: 10
+  },
+  loading: {
+    position: "absolute",
+    top: 10,
+    right: 10
   }
 });
