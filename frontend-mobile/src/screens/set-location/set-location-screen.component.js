b47d9d1aea1db76ea7c9f5add8e635601fdb58b2
Kane Clover <baron.vonkanehoffen@gmail.com>
2020-03-12 14:50:33 +0000

    Set location screen styling

--- frontend-mobile/src/screens/set-location/set-location-screen.component.js
+++ frontend-mobile/src/screens/set-location/set-location-screen.component.js
@@ -1,24 +1,23 @@
 import React, { useContext } from "react";
 import { View } from "react-native";
-import {
-  Layout,
-  Text,
-} from "@ui-kitten/components";
+import { Layout, Text, useTheme } from "@ui-kitten/components";
 import { AuthContext } from "../auth/auth-provider.component";
 import { useMutation } from "@apollo/react-hooks";
 import * as queries from "../../data-layer/graphql-queries";
 import { StyleSheet } from "react-native";
-import {PostcodeLookup} from './postcode-lookup.component';
-import {GetDeviceLocation} from './get-device-location.component';
-import Snackbar from 'react-native-snackbar';
+import { PostcodeLookup } from "./postcode-lookup.component";
+import { GetDeviceLocation } from "./get-device-location.component";
+import Snackbar from "react-native-snackbar";
+import { PaLogo } from "../../components/pa-logo.component";
 
 export function SetLocationScreen({ navigation }) {
+  const theme = useTheme();
   const { credentials } = useContext(AuthContext);
   const [updateUserLocationMutation, { loading, error, data }] = useMutation(
     queries.UPDATE_USER_LOCATION
   );
 
-  const updateUserLocation = async (coordinates) => {
+  const updateUserLocation = async coordinates => {
     await updateUserLocationMutation({
       variables: {
         id: credentials.claims.sub,
@@ -28,7 +27,7 @@ export function SetLocationScreen({ navigation }) {
         }
       }
     });
-    console.log('done update loc', coordinates);
+    console.log("done update loc", coordinates);
     navigation.navigate("Home");
   };
 
@@ -42,19 +41,20 @@ export function SetLocationScreen({ navigation }) {
   return (
     <Layout style={styles.container}>
       <View>
+        <PaLogo color={theme["color-primary-300"]} size={140} />
         <Text category="h3" style={styles.intro}>
           Where do you live?
         </Text>
-        <Text category="h3">
+        <Text>
           We'll show you planning applications that have been made near you.
         </Text>
       </View>
       <View>
-        <PostcodeLookup updateUserLocation={updateUserLocation}/>
+        <PostcodeLookup updateUserLocation={updateUserLocation} />
         <Text category="h4" style={styles.or}>
           or
         </Text>
-        <GetDeviceLocation updateUserLocation={updateUserLocation}/>
+        <GetDeviceLocation updateUserLocation={updateUserLocation} />
       </View>
     </Layout>
   );
@@ -67,6 +67,7 @@ const styles = StyleSheet.create({
     padding: 20
   },
   intro: {
+    paddingTop: 20,
     paddingBottom: 20
   },
   or: {
