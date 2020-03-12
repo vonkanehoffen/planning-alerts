05bc847cd28ef0353bc691eb0eb28a5492b468a3
Kane Clover <baron.vonkanehoffen@gmail.com>
2020-03-12 14:46:47 +0000

    Settings screen styling

--- frontend-mobile/src/components/pa-logo.component.js
+++ frontend-mobile/src/components/pa-logo.component.js
@@ -1,14 +1,27 @@
 import React from "react";
 import Svg, { G, Path } from "react-native-svg";
 
-export function PaLogo() {
+export function PaLogo({ color = "#FFFFFF", size = 200 }) {
   return (
-      <Svg width={200} height={138} viewBox="0 0 398 276">
-        <G id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-          <G id="pa-logo" transform="translate(-1.000000, -55.000000)" fill="#FFFFFF">
-            <Path d="M253.209994,55 L253.209994,128.932248 L199.999136,55.1808015 L1,331 L399,331 L306.707852,203.080766 L306.707852,92.6188261 L253.210858,55 L253.209994,55 Z M199.998705,130 L314,288 L86,288 L199.998705,130 Z" id="Fill-1"></Path>
-          </G>
+    <Svg width={size} height={size * 0.69} viewBox="0 0 398 276">
+      <G
+        id="Page-1"
+        stroke="none"
+        stroke-width="1"
+        fill="none"
+        fill-rule="evenodd"
+      >
+        <G
+          id="pa-logo"
+          transform="translate(-1.000000, -55.000000)"
+          fill={color}
+        >
+          <Path
+            d="M253.209994,55 L253.209994,128.932248 L199.999136,55.1808015 L1,331 L399,331 L306.707852,203.080766 L306.707852,92.6188261 L253.210858,55 L253.209994,55 Z M199.998705,130 L314,288 L86,288 L199.998705,130 Z"
+            id="Fill-1"
+          ></Path>
         </G>
-      </Svg>
+      </G>
+    </Svg>
   );
 }
--- frontend-mobile/src/screens/settings/settings-screen.component.js
+++ frontend-mobile/src/screens/settings/settings-screen.component.js
@@ -1,20 +1,48 @@
 import React from "react";
-import { Alert } from "react-native";
-import { Button, Icon, Layout, Text } from "@ui-kitten/components";
+import { StyleSheet, View } from "react-native";
+import { Icon, Layout, ListItem, Text, useTheme } from "@ui-kitten/components";
 import { AuthContext } from "../auth/auth-provider.component";
-import { auth0 } from "../auth/auth-provider.component";
-import * as Keychain from "react-native-keychain"
+import { PaLogo } from "../../components/pa-logo.component";
+
+const PersonIcon = style => <Icon {...style} name="person-outline" />;
+const LogoutIcon = style => <Icon {...style} name="log-out-outline" />;
+const NavigationIcon = style => <Icon {...style} name="navigation-2-outline" />;
 
 export function SettingsScreen({ navigation }) {
-  const { doLogout } = React.useContext(AuthContext);
+  const theme = useTheme();
+  const {
+    doLogout,
+    credentials: { claims }
+  } = React.useContext(AuthContext);
 
   return (
-    <Layout style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Settings Screen</Text>
-      <Button onPress={doLogout}>Log Out</Button>
-      <Button onPress={() => navigation.navigate("Set Location")}>
-        Set Location
-      </Button>
+    <Layout style={styles.container}>
+      <View style={styles.header}>
+        <PaLogo color={theme["color-primary-300"]} size={140} />
+      </View>
+      <Text category="h1" style={styles.header}>
+        Settings
+      </Text>
+      <ListItem
+        title={`Logged in as ${claims.name}`}
+        description={claims.sub}
+        icon={PersonIcon}
+      />
+      <ListItem
+        title="Set Location"
+        onPress={() => navigation.navigate("Set Location")}
+        icon={NavigationIcon}
+      />
+      <ListItem title="Log out" icon={LogoutIcon} onPress={doLogout} />
     </Layout>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  header: {
+    margin: 20
+  }
+});
