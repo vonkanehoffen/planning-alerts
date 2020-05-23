e2e98b784ddefdcddd187c2777e8bff43a1c5a13
Kane Clover <baron.vonkanehoffen@gmail.com>
2020-05-23 10:36:23 +0100

    Upgrade deps and config to UI Kitten 5 (TS)

--- frontend-mobile/metro.config.js
+++ frontend-mobile/metro.config.js
@@ -5,13 +5,22 @@
  * @format
  */
 
-module.exports = {
+const MetroConfig = require("@ui-kitten/metro-config");
+
+/**
+ * @see https://akveo.github.io/react-native-ui-kitten/docs/guides/improving-performance
+ */
+const evaConfig = {
+  evaPackage: "@eva-design/eva"
+};
+
+module.exports = MetroConfig.create(evaConfig, {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
-};
+        inlineRequires: false
+      }
+    })
+  }
+});
