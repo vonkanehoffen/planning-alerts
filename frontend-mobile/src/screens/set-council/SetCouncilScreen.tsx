9cd62e1aa46d521e641ac63d174901cf3f905daf
Kane Clover <baron.vonkanehoffen@gmail.com>
2020-05-31 14:25:28 +0100

    Debounce autocomplete

--- frontend-mobile/src/screens/set-council/SetCouncilScreen.tsx
+++ frontend-mobile/src/screens/set-council/SetCouncilScreen.tsx
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Input, Layout, Text, useTheme } from '@ui-kitten/components'
 import { StyleSheet } from 'react-native'
 import { useCouncil_AutocompleteLazyQuery } from '../../generated/graphql'
 import { FullScreenLoader } from '../../components/FullScreenLoader'
+import useDebounce from '../../hooks/use-debounce';
 
 interface SetCouncilScreenProps {
 
 }
 
 export const SetCouncilScreen: React.FC<SetCouncilScreenProps> = ({}) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
   const [loadCouncilAutocomplete, { called, loading, data, error }] = useCouncil_AutocompleteLazyQuery();
+
+  useEffect(
+    () => {
+      if(debouncedSearchTerm) {
+        loadCouncilAutocomplete({ variables: { input: `%${debouncedSearchTerm}%`}});
+      }
+    }, [debouncedSearchTerm]
+  );
 
   console.log(data, error);
   return (
     <Layout style={styles.container}>
       <Text category="h3">Who are your Local Council?</Text>
-      <Input label="Council" onChangeText={s => loadCouncilAutocomplete({ variables: { input: `%${s}%`}})}/>
+      <Input label="Council" onChangeText={s => setSearchTerm(s)}/>
       {(called && loading) && <Text>Loading Lazy Auto</Text>}
       <Text category="p1">{JSON.stringify(data?.council, null, 2)}</Text>
     </Layout>
